4539c6486e32b1bca027d09c51f34ce85147dc16
Kyle Spraggs <theman@spiffyjr.me>
2015-06-30 22:30:12 -0500

    Initial working commit

--- src/ResponseCache.php
+++ src/ResponseCache.php
@@ -1,21 +1,27 @@
 <?php
 namespace Tonis\ResponseCache;
 
+use Doctrine\Common\Cache\Cache;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
-use Psr\Log\LoggerInterface;
 
 final class ResponseCache
 {
-    /** @var LoggerInterface */
-    private $logger;
+    /** @var Cache */
+    private $cache;
+    /** @var array */
+    private $config;
 
     /**
-     * @param LoggerInterface $logger
+     * @param Cache $cache
+     * @param array $config
      */
-    public function __construct(LoggerInterface $logger)
+    public function __construct(Cache $cache, array $config = [])
     {
-        $this->logger = $logger;
+        $defaults = ['ttl' => 60];
+
+        $this->cache  = $cache;
+        $this->config = array_merge($defaults, $config);
     }
 
     /**
@@ -26,8 +32,48 @@ final class ResponseCache
      */
     public function __invoke(ServerRequestInterface $request, ResponseInterface $response, callable $next)
     {
+        $key  = $this->generateKey($request);
+        $data = $this->cache->fetch($key);
+
+        if (false !== $data) {
+            list($body, $code, $headers) = unserialize($this->cache->fetch($key));
+
+            $response->getBody()->write($body);
+            $response = $response->withStatus($code);
+
+            foreach (unserialize($headers) as $name => $value) {
+                $response = $response->withHeader($name, $value);
+            }
+
+            return $response;
+        }
+
         $response = $next ? $next($request, $response) : $response;
 
+        // save cache - status code, headers, body
+        $body    = $response->getBody()->__toString();
+        $code    = $response->getStatusCode();
+        $headers = serialize($response->getHeaders());
+        $data    = serialize([$body, $code, $headers]);
+
+        $this->cache->save($key, $data, $this->config['ttl']);
+
         return $response;
+    }
+
+    /**
+     * Generates a cache key based on a unique request.
+     *
+     * @todo determine parts required for unique request
+     * @param ServerRequestInterface $request
+     * @return string
+     */
+    private function generateKey(ServerRequestInterface $request)
+    {
+        $parts = [
+            $request->getMethod(),
+            $request->getUri()->getPath()
+        ];
+        return implode(':', $parts);
     }
 }
